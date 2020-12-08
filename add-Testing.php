a905063febcf5a1d9a9770cd05f500357ceea187
AminHussaini <59252100+AminHussaini@users.noreply.github.com>
2020-12-09 03:40:00 +0500

    Update add-Testing.php

--- add-Testing.php
+++ add-Testing.php
@@ -22,7 +22,7 @@ if ($_SESSION["user_role"] == "SRS") echo $return_var;
         </ol>
       </nav>
     </div>
-   
+
     <div class="col-xl-12">
       <div class="ms-panel">
         <div class="ms-panel-header ms-panel-custome">
@@ -34,87 +34,89 @@ if ($_SESSION["user_role"] == "SRS") echo $return_var;
 
             $sql = "SELECT * FROM sendfortest";
             $result  = mysqli_query($con, $sql) or die("query fail");
-            
-            ?>
-            <table id="add-testing-product" class="table w-100 thead-primary">
-              <thead>
-                <tr>
-                  <th>
-                    Id
-                  </th>
-                  <th>
-                    User Name
-                  </th>
-                  <th>
-                    Product Category
-                  </th>
-                  <th>
-                    Product Name
-                  </th>
-                  <th>
-                    Product Code
-                  </th>
-                  <th>
-                    Product Detail
-                  </th>
-                  <th>
-                    Date
-                  </th>
-                  <th>
-                    Testing Type
-                  </th>
-                  <th class="text-center">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
 
-                <?php
-                if ($result = mysqli_query($con, $sql)) {
-                  $i = 1;
-                  while ($getsenddeatilrow= mysqli_fetch_assoc($result)) {
-                    echo "<tr>
+            $testingrow = 'SELECT * FROM testingtypes';
+            if (mysqli_num_rows(mysqli_query($con, $testingrow)) > 0) { ?>
+              <table id="add-testing-product" class="table w-100 thead-primary">
+                <thead>
+                  <tr>
+                    <th>
+                      Id
+                    </th>
+                    <th>
+                      User Name
+                    </th>
+                    <th>
+                      Product Category
+                    </th>
+                    <th>
+                      Product Name
+                    </th>
+                    <th>
+                      Product Code
+                    </th>
+                    <th>
+                      Product Detail
+                    </th>
+                    <th>
+                      Date
+                    </th>
+                    <th>
+                      Testing Type
+                    </th>
+                    <th class="text-center">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  <?php
+                  if ($result = mysqli_query($con, $sql)) {
+                    $i = 1;
+                    while ($getsenddeatilrow = mysqli_fetch_assoc($result)) {
+
+                      echo "<tr>
                         <td>" . $i . "</td>
                         ";
 
-                    $sql = "SELECT * FROM register where id=" . $getsenddeatilrow['sendbyuser'] . "";
-                    $getRegister = mysqli_query($con, $sql);
-                    $getRegisterRow = mysqli_fetch_assoc($getRegister);
+                      $sql = "SELECT * FROM register where id=" . $getsenddeatilrow['sendbyuser'] . "";
+                      $getRegister = mysqli_query($con, $sql);
+                      $getRegisterRow = mysqli_fetch_assoc($getRegister);
 
-                    echo   "<td>" . $getRegisterRow['name'] . "</td>";
-                    
-                    $sqlp = "SELECT * FROM product where ProductId= ".$getsenddeatilrow["productid"] ."";
-                    $presult = mysqli_query($con, $sqlp) or die("query fail");
-                    $productrow=mysqli_fetch_array($presult);
-        
+                      echo   "<td>" . $getRegisterRow['name'] . "</td>";
 
-                    $productCategory = "SELECT * FROM producttype where ProductTypeId=" . $productrow['ProductIdType'] . "";
-                    $getProductCategory = mysqli_query($con, $productCategory);
-                    $getProductCategoryRow = mysqli_fetch_assoc($getProductCategory);
-                    
-                    echo "
+                      $sqlp = "SELECT * FROM product where ProductId= " . $getsenddeatilrow["productid"] . "";
+                      $presult = mysqli_query($con, $sqlp) or die("query fail");
+                      $productrow = mysqli_fetch_array($presult);
+
+
+                      $productCategory = "SELECT * FROM producttype where ProductTypeId=" . $productrow['ProductIdType'] . "";
+                      $getProductCategory = mysqli_query($con, $productCategory);
+                      $getProductCategoryRow = mysqli_fetch_assoc($getProductCategory);
+
+                      echo "
                         <td class='product-name' style='text-transform:capitalize;'>" . $getProductCategoryRow['ProductName']  . "</td>
-                        <td>" .$productrow['ProductName'] . "</td>
+                        <td>" . $productrow['ProductName'] . "</td>
                         <td>" . $productrow['ProductCode'] . "</td>
                         <td style='min-width: 200px;max-width: 280px;word-break: break-word;white-space: initial;'>" . $productrow['ProductDetail'] . "</td>
                         <td style='min-width: 170px;white-space: initial;'> <b>Date: </b>" . explode(" ", $productrow['ProductDate'])[0] . "<br> <b>Time: </b>" . explode(" ", $productrow['ProductDate'])[1] . "</td>";
-                       
-                    echo "<td style='min-width: 200px;max-width: 280px;word-break: break-word;'>
+
+                      echo "<td style='min-width: 200px;max-width: 280px;word-break: break-word;'>
                     <div class='input-group'>
                     <select class='form-control' id='addTestProductCategory' name='addTestProductCategory' required>
                     <option value='empty' selected disabled>Select</option>";
-                    
-                    $sqlf = 'SELECT * FROM producttype';
 
-                    if ($results = mysqli_query($con, $sqlf)) {
-                      
-                      while ($row = mysqli_fetch_assoc($results)) {
-                        echo "<option style='text-transform:lowercase; name='. $row[ProductTypeId] .' value= ' . $row[ProductTypeId] .' > . $row[ProductName] . </option>";
+                      $sqlf = 'SELECT * FROM testingtypes';
+
+                      if ($results = mysqli_query($con, $sqlf)) {
+
+                        while ($row = mysqli_fetch_assoc($results)) {
+                          echo "<option style='text-transform:lowercase; name='$row[TestingTypeID]' value='$row[TestingTypeID]' > $row[TestingTypeName] </option>";
+                        }
                       }
-                    }
-                    
-                     echo" 
+
+                      echo "
                       </select>
                      </div>
                      </td>
@@ -123,12 +125,17 @@ if ($_SESSION["user_role"] == "SRS") echo $return_var;
                         </td>
                         </tr>
                         ";
-                    $i++;
+                      $i++;
+                    }
                   }
-                }
-                ?>
-              </tbody>
-            </table>
+                  ?>
+                </tbody>
+              </table>
+            <?php  } else  {
+             echo "<h6 class='text-center' style='color:var(--red);font-weight:700'>Add the testing First</h6>";
+            };
+            ?>
+
           </div>
         </div>
       </div>
@@ -137,5 +144,3 @@ if ($_SESSION["user_role"] == "SRS") echo $return_var;
 </div>
 
 <?php include "inc/footer.php" ?>
-
-
